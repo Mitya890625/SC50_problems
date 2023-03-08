2acf5b671fa79a6087b5b1bc58b935cbef4eea4b
Mitya Zhdanovich <dmitriyzhdan23@gmail.com>
2023-03-08 13:22:28 +0300

    Changes added

--- Week1_Conditionals/bank.py
+++ Week1_Conditionals/bank.py
@@ -1,13 +1,13 @@
 def main():
 
     greetings = input("Greeting:")
-    greetings = greetings.strip().lower().split()
-    first_letter = list(greetings[0])
+    greetings = greetings.strip().lower().split() #split input string and lower case of it
+    first_letter = list(greetings[0]) #makes list from string
     print(greetings)
     print(first_letter)
-    if greetings[0] == "hello":
+    if greetings[0] == "hello": #checks if first word is "hello"
         print("0$")
-    elif first_letter[0] == 'h':
+    elif first_letter[0] == 'h': #checks if first letter is "h"
         print("20$")
     else:
         print("100$")
